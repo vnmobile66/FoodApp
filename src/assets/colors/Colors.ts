a17bbb668e67f14372f551346890a5b11a741a28
Ngoc Phuc <tphuc7891@gmail.com>
2025-05-10 09:55:58 +0700

    foodapp s1

---
+++ src/assets/colors/Colors.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+    'LinearRedStart': '#FF939B', // linear-red-start
+    'LinearRedPrimary': '#EF2A39', // primary 67%
+    'White': '#fff', // white
+    'Active': '#EF2A39', // active
+    'Unactive': '#6A6A6A', // unactive
+    'UnactiveText': '#F3F4F6', // unactive-text
+    'ActiveText': '#F5F5F5', // active-text
+    'ColorTextProduct': '#3C2F2F', // color-text-product
+    'Star': '#FF9633', // star
+    'OrderButton': '#3C2F2F', // order-button
+};
---
+++ src/screens/splashScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function splashScreen() {
+  return (
+    <View>
+      <Text>splashScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
